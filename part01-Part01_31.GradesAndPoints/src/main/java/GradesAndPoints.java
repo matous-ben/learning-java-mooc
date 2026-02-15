fc522c68ff01b88a5d7bec539f18f6c0fe719e8d
mat-ben <matous.benedikt@gmail.com>
2026-02-15 21:50:02 +0100

    only one easy exercise for today :)

--- part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
+++ part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -5,6 +5,25 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Give points [0-100]:");
+        int mark = scan.nextInt();
 
+        if (mark < 0) {
+            System.out.println("Grade: impossible!");
+        } else if (mark >= 0 && mark <= 49) {
+            System.out.println("Grade: failed");
+        } else if (mark >= 50 && mark <= 59) {
+            System.out.println("Grade: 1");
+        } else if (mark >= 60 && mark <= 69) {
+            System.out.println("Grade: 2");
+        } else if (mark >= 70 && mark <= 79) {
+            System.out.println("Grade: 3");
+        } else if (mark >= 80 && mark <= 89) {
+            System.out.println("Grade: 4");
+        } else if (mark >= 90 && mark <= 100) {
+            System.out.println("Grade: 5");
+        } else if (mark > 100) {
+            System.out.println("Grade: incredible!");
+        }
     }
 }
