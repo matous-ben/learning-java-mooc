f2dd686713ce71be660f7dc75253746747d48af0
mat-ben <matous.benedikt@gmail.com>
2026-05-05 12:57:05 +0200

    started the Part02 + config the tmc extension

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,12 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Declaring the variable and assigning user input
+        int number = scanner.nextInt();
+
+        // Identifying the operation
+        int square = number * number;
+
+        System.out.println(square);
     }
 }
